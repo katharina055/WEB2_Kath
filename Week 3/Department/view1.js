b629521d7e39ba0f9c49eb614c7850a2ebfe3edb
Katharina Aryani <Mirrah@Mirrahs-MacBook-Pro-2.local>
2016-05-29 10:30:34 +0200

    Week 2 and Week 1 + rootSite
    Week  2 assignment

---
+++ Week 3/Department/view1.js
@@ -0,0 +1,11 @@
+'use strict';
+var Department = angular.module('myAppDepartment', []);
+
+app.controller("myController", function($scope){
+	$scope.name="Marketing";
+	$scope.country="Netherlands";
+	$scope.employees= ["Katharina", "Andrea", "Max"];
+	
+	
+});
+
